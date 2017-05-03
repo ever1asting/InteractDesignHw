979eb07d00320eba4b2fcd7cd3ab9e470f7c1384
ywn <312369144@qq.com>
2017-05-03 21:25:52 +0800

    modify sound light

--- SoundAndLight/SoundAndLight.ino
+++ SoundAndLight/SoundAndLight.ino
@@ -6,18 +6,18 @@ Adafruit_NeoPixel ColorLED = Adafruit_NeoPixel(1, 6, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
-  pinMode(A2, INPUT); 
-  pinMode(A4, INPUT); 
+  pinMode(A0, INPUT);
+  pinMode(A2, INPUT);
   ColorLED.begin();
-  Serial.begin(9600); 
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  light_state = analogRead(A2); 
-  sound_state = analogRead(A4); 
-  if (light_state < 200 && sound_state > 600) {
-    ColorLED.setPixelColor(0, ColorLED.Color(255, 255, 255));
+  light_state = analogRead(A0);
+  sound_state = analogRead(A2);
+  if (light_state < 170 && sound_state > 600) {
+    ColorLED.setPixelColor(0, ColorLED.Color(55, 55, 55));
     ColorLED.show();
     delay(5000);
   }
@@ -25,4 +25,9 @@ void loop()
     ColorLED.setPixelColor(0, ColorLED.Color(0, 0, 0));
     ColorLED.show();
   }
+  Serial.print("light_state:");
+  Serial.print(light_state);
+  Serial.print(".....sound_state:");
+  Serial.println(sound_state);
+  delay(1000); 
 }
